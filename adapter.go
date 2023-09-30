fa8b195e38051b28312669a76da69db29b3ab3c8
timtoronto634 <tim225x@gmail.com>
2023-09-30 16:02:34 +0900

    feat: add adapter

---
+++ adapter.go
@@ -0,0 +1,48 @@
+package pgxslog
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/tracelog"
+)
+
+type Logger struct {
+	l *slog.Logger
+}
+
+func NewTracer(l *slog.Logger) pgx.QueryTracer {
+	return &tracelog.TraceLog{
+		Logger:   &Logger{l: l},
+		LogLevel: tracelog.LogLevelTrace,
+	}
+}
+
+func (l *Logger) Log(ctx context.Context, level tracelog.LogLevel, msg string, data map[string]any) {
+	logger := l.l
+	attrs := make([]slog.Attr, 0, len(data))
+	for k, v := range data {
+		attrs = append(attrs, slog.Any(k, v))
+	}
+	logger.LogAttrs(ctx, translateLevel(level), msg, attrs...)
+}
+
+func translateLevel(level tracelog.LogLevel) slog.Level {
+	switch level {
+	case tracelog.LogLevelTrace:
+		return slog.LevelDebug
+	case tracelog.LogLevelDebug:
+		return slog.LevelDebug
+	case tracelog.LogLevelInfo:
+		return slog.LevelInfo
+	case tracelog.LogLevelWarn:
+		return slog.LevelWarn
+	case tracelog.LogLevelError:
+		return slog.LevelError
+	case tracelog.LogLevelNone:
+		return slog.LevelError
+	default:
+		return slog.LevelError
+	}
+}
